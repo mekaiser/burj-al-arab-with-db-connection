5f5bfe189bfed27cf22a51a409ff0801f4b5ff76
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-06-19 18:53:39 +0600

    done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,7 +11,6 @@ const Login = () => {
   const location = useLocation();
   const { from } = location.state || { from: { pathname: "/" } };
 
-
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
   }
@@ -24,8 +23,8 @@ const Login = () => {
         /** @type {firebase.auth.OAuthCredential} */
         const { displayName, email } = result.user;
         const signedInUser = { name: displayName, email };
-        console.log(signedInUser);
         setLoggedInUser(signedInUser);
+        storeAuthToken();
         history.replace(from);
       })
       .catch((error) => {
@@ -39,6 +38,19 @@ const Login = () => {
         // ...
       });
   };
+
+  const storeAuthToken = () => {
+    firebase
+      .auth()
+      .currentUser.getIdToken(/* forceRefresh */ true)
+      .then(function (idToken) {
+        sessionStorage.setItem("token", idToken);
+      })
+      .catch(function (error) {
+        // Handle error
+      });
+  };
+
   return (
     <div>
       <h1>This is Login</h1>
